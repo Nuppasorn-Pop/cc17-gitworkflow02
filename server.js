3c3a0b167cdfe31532270a16f8963ca64efa9220
nattawanoyster <nattawan.prs@gmail.com>
2024-06-19 14:08:56 +0700

    login

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.use("/login", (req, res, next) => {
+  res(200).json({ message });
+});
+
 const port = 8000;
 app.listen(port, () => {
   console.log("server on", port);
