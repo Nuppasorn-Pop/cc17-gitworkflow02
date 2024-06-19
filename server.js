2aa23b0be133233632f3a20b8996395b5f7fdd28
PunthiraChin <punthira.chino@gmail.com>
2024-06-19 14:06:00 +0700

    getUserRouter created

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { notfoundMiddleware } = require("./middleware/notfound-middleware");
+const getUserRouter = require("./routes/getuser-route");
 const app = express();
 
+app.use(getUserRouter);
 app.use(notfoundMiddleware);
 const port = 8000;
 app.listen(port, () => {
