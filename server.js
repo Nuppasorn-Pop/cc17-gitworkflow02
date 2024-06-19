791696413afc99bb870d65c7b847324c48e4438b
Nuppasorn-Pop <nuppasorn.s@gmail.com>
2024-06-19 14:05:29 +0700

    register

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { notfoundMiddleware } = require("./middleware/notfound-middleware");
+const registerRouter = require("./routes/register-route");
 const app = express();
 
+app.use("/register", registerRouter);
 app.use(notfoundMiddleware);
 const port = 8000;
 app.listen(port, () => {
